3b606a2063c1b03f4bbbf3428c6bb3fbc1e6f86d
franckps <fran.p.s@hotmail.com>
2024-04-12 01:56:29 -0300

    feat: implement service to trigger password recovery

---
+++ src/services/recoveryPassword.ts
@@ -0,0 +1,25 @@
+import { CustomError } from "../utils/errors";
+import { UserRepository } from "../interfaces/repository";
+import {
+  PasswordRecoveryGenerate,
+  SendRecoveryToken,
+} from "../interfaces/utils";
+
+export class RecoveryPassword {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly passwordRecoveryGenerate: PasswordRecoveryGenerate,
+    private readonly sendRecoveryToken: SendRecoveryToken
+  ) {}
+
+  async execute(username: string): Promise<void> {
+    const user = await this.userRepository.getByUsername(username);
+    if (!user) throw new CustomError("User not found");
+    const { passwordRecoveryToken, passwordRecoveryExpiresIn } =
+      this.passwordRecoveryGenerate.generateRecovery();
+    user.passwordRecoveryToken = passwordRecoveryToken;
+    user.passwordRecoveryExpiresIn = passwordRecoveryExpiresIn;
+    this.userRepository.updateByUsername(username, user);
+    this.sendRecoveryToken.sendRecovery(passwordRecoveryToken);
+  }
+}
