8032c36f54b65edf6cef7a8ae7886573522ec24d
franckps <fran.p.s@hotmail.com>
2024-04-12 21:32:31 -0300

    feat: implement method to compare password with a hash

--- src/utils/passwordEncryptBcrypt.ts
+++ src/utils/passwordEncryptBcrypt.ts
@@ -1,7 +1,18 @@
-import { genSalt, hash } from "bcrypt";
-import { PasswordEncrypt } from "../interfaces/utils";
+import { genSalt, hash, compare } from "bcrypt";
+import { PasswordEncrypt, PasswordValidator } from "../interfaces/utils";
 
-export class PasswordEncryptBcrypt implements PasswordEncrypt {
+export class PasswordEncryptBcrypt
+  implements PasswordEncrypt, PasswordValidator
+{
+  isEqual(passwordToTest: string, userPassword: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      compare(passwordToTest, userPassword, (err, result) => {
+        if (!!err) return reject(err);
+        return resolve(result);
+      });
+    });
+  }
+
   encrypt(password: string): Promise<string> {
     return new Promise((resolve, reject) => {
       genSalt(10, "a", (err, saltResult) => {
