c6967a458f8024074d589de9e67bd7633658890d
franckps <fran.p.s@hotmail.com>
2024-04-14 03:02:10 -0300

    config: add sequelize config

---
+++ migrations/20240414035351-first_migrate.js
@@ -0,0 +1,106 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      userId: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+      image: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.NOW,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.NOW,
+        allowNull: false,
+      },
+      passwordRecoveryToken: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+      passwordRecoveryExpiresIn: {
+        type: Sequelize.DataTypes.NUMBER,
+        allowNull: true,
+      },
+    });
+    await queryInterface.createTable("Authentications", {
+      id: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      code: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      codeExpiresIn: {
+        type: Sequelize.DataTypes.NUMBER,
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.NOW,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.NOW,
+        allowNull: false,
+      },
+      expiresIn: {
+        type: Sequelize.DataTypes.NUMBER,
+        allowNull: false,
+      },
+      isActive: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+    });
+    await queryInterface.addConstraint("Authentications", {
+      fields: ["userId"],
+      type: "FOREIGN KEY",
+      name: "FK_Authentications_Users",
+      references: {
+        table: "Users",
+        field: "userId",
+      },
+      onDelete: "no action",
+      onUpdate: "no action",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint(
+      "Authentications",
+      "FK_Authentications_Users"
+    );
+    await queryInterface.dropTable("Authentications");
+    await queryInterface.dropTable("Users");
+  },
+};
